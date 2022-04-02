f6da3117db52e9f86086c40ef725bfa532433611
delan <delan.april@gmail.com>
2022-04-02 15:27:19 +0800

    damn losted

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Create from "./pages/Create";
 import Seed from "./pages/Seed";
 
 function App() {
   return (
     <div className="App">
-      <Seed /> 
+      <Create /> 
     </div>
   );
 }
