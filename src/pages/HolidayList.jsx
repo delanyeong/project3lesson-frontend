7acacda1840abfc304c4b5210243df6ea92c9543
delan <delan.april@gmail.com>
2022-04-02 16:34:24 +0800

    delete button

--- src/pages/HolidayList.jsx
+++ src/pages/HolidayList.jsx
@@ -6,17 +6,29 @@ function HolidayList() {
   const [holidays, setHolidays] = useState([]);
 
   useEffect(() => {
-    fetch(urlcat(BACKEND, "/api/holidays/seed"))
+    fetch(urlcat(BACKEND, "/api/holidays/"))
       .then((response) => response.json())
       .then((data) => setHolidays(data));
   }, []);
 
+  const handleDelete = (id) => () => {
+    const url = urlcat(BACKEND, `/api/holidays/${id}`);
+    fetch(url, { method: "DELETE" })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  };
+
   return (
     <>
+        <pre>
+            {JSON.stringify(holidays, null, 2)}
+        </pre>
       <ul>
         {holidays.map((holiday) => (
-          <li key={holiday.id}>
-            <a href="#">{holiday.name}</a>
+          <li key={holiday._id}>
+            <a href="/">{holiday.name}</a>
+            {" -- "}
+            <span onClick={handleDelete(holiday._id)}>Delete</span>
           </li>
         ))}
       </ul>
